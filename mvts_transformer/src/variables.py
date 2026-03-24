e26ca8b5adcddc2c02fc7c12aea89a3905e55990
zeyuD <zeyud9@outlook.com>
2026-03-24 09:25:49 -0500

    test

--- mvts_transformer/src/variables.py
+++ mvts_transformer/src/variables.py
@@ -18,10 +18,10 @@ vote_list = ["A", "B", "C", "D", "E"]
 num_try = 10
 num_round = 100
 
-# user_list = ["Chuan", "Gujing", "Haofan", "Jimmy", "Jingwei", 
-#              "Junwei", "minglei", "Minjie", "Mingxuan","Rosie", 
-#              "Sihang", "Wen", "Yirui", "Zeyu", "Zidan", 
-#              "Ziyue1"]
+user_list = ["Chuan", "Gujing", "Haofan", "Jimmy", "Jingwei", 
+             "Junwei", "minglei", "Minjie", "Mingxuan","Rosie", 
+             "Sihang", "Wen", "Yirui", "Zeyu", "Zidan", 
+             "Ziyue1"]
 # user_list = ["Chuan", "Gujing", "Haofan", "Junwei", "Yirui"]
 # user_list = ["Zeyu"]
 # user_list = ["ZeyuZoom"]
@@ -30,8 +30,8 @@ num_round = 100
 # user_othersetting_list = ["Jingwei", "minglei", "Minjie", "Wen", "Zeyu"]
 # user_othersetting_list = ["ZeyuZoom"]
 
-user_list = ["JingweiZoom", "mingleiZoom", "MinjieZoom", "WeiZoom", "WenZoom", "ZeyuZoom"]
-user_othersetting_list = ["JingweiZoom", "mingleiZoom", "MinjieZoom", "WeiZoom", "WenZoom", "ZeyuZoom"]
+# user_list = ["JingweiZoom", "mingleiZoom", "MinjieZoom", "WeiZoom", "WenZoom", "ZeyuZoom"]
+# user_othersetting_list = ["JingweiZoom", "mingleiZoom", "MinjieZoom", "WeiZoom", "WenZoom", "ZeyuZoom"]
 
 grid_dim = "22"
 
